6b83f3e23ec0a5d226c7f7e20c72b51761b9eabb
gauravDombale <gauravdombale007@gmail.com>
2025-10-21 13:02:00 +0530

    utility functions added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
 //* Connect to the database
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  });
