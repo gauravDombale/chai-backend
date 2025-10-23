cc0f24bc2e4cbe47474f82ad8f7fae9f8630b079
gauravDombale <gauravdombale007@gmail.com>
2025-10-23 13:34:44 +0530

    unlink file on successfull file upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,6 +20,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     //file has been uploaded
     console.log("file uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
